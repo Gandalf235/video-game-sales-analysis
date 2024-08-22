17505a1e5f53622f9d8f42715222244b502cafd9
Zelieren <86133929+Gandalf235@users.noreply.github.com>
2024-08-21 23:36:13 -0600

    Add files via upload

---
+++ sql/vgsales2015_2024_08_21_V01.sql
@@ -0,0 +1,24 @@
+-- Tabla temporal que extrae el año y el mes
+-- Selecciona todas las columnas de la tabla ventas
+WITH game_year_sales AS(
+  SELECT
+  EXTRACT(YEAR FROM release_date) AS year, -- Extrae el año de la fecha de lanzamiento
+  EXTRACT(MONTH FROM release_date) AS month, -- Extrae el mes de la fecha de lanzamiento
+  *
+FROM
+  `videogames_sales.sales`
+)
+
+-- Consulta que filtra el año y las consolas del analisis
+SELECT
+  title, -- Nombre del juego
+  genre, -- Genero del juego
+  console, -- Consola en que se lanzo
+  year, -- Año de lanzamiento
+  month, -- Mes de lanzamiento
+  total_sales -- Total de ventas
+FROM
+  game_year_sales
+WHERE
+  (year = 2015) -- Filtra el año en 2015
+  AND (console = 'PS4' OR  console = 'XOne') -- Filtro para consolas XOne o PS4
